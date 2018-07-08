38045a62ee9556cdf8d2c6553a47a48a54d49755
Jedshady <wendelljiang.au@gmail.com>
2018-07-08 21:44:32 +1000

    Add problem 14

---
+++ Problem 1-30/problem_14.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+'''
+This problem was asked by Google.
+
+The area of a circle is defined as πr^2. Estimate π to 3 decimal places using a
+Monte Carlo method.
+
+Hint: The basic equation of a circle is x2 + y2 = r2.
+'''
+import random
+
+def est_pi(num):
+    point = list()
+    cnt = 0
+    total = 0
+    prev_pi, pi_approx = 0, 3
+
+    while True:
+        for i in range(num):
+            random.seed(total) # set seed
+            point.append((random.random(), random.random()))
+            if point[i][0] ** 2 + point[i][1] ** 2 <= 1:
+                cnt += 1
+            total += 1
+        prev_pi = pi_approx
+        pi_approx = 4 * cnt / float(total)
+        if abs(prev_pi - pi_approx) < 1e-8:
+            return i, pi_approx
+
+
+def main():
+    print est_pi(10000)
+
+
+if __name__ == '__main__':
+    main()
--- Problem Descriptions.md
+++ Problem Descriptions.md
@@ -8,6 +8,13 @@
 ## <a id='1-30'></a> Problem 1-30
 
 ----
+### Problem 14
+This problem was asked by Google.
+
+The area of a circle is defined as ```πr^2```. Estimate π to 3 decimal places using a Monte Carlo method.
+
+Hint: The basic equation of a circle is ```x2 + y2 = r2```.
+
 ### Problem 13
 This problem was asked by Amazon.
 
