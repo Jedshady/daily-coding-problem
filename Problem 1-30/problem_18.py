73b7e992c62c4449d96f06f2a9ed4485cf89507a
Jedshady <wendelljiang.au@gmail.com>
2018-07-12 10:13:18 +1000

    modify comments

--- Problem 1-30/problem_18.py
+++ Problem 1-30/problem_18.py
@@ -26,14 +26,10 @@ from collections import deque
 # method for printing maximum element
 # of all subarrays of size k
 def printMax(arr, n, k):
-    '''Create a Double Ended Queue, q that
-    will store indexes of array elements.
-    The queue will store indexes of useful
-    elements in every window and it will
-    maintain decreasing order of values from
-    front to rear in q, i.e., arr[q.front[]]
-    to arr[q.rear()] are sorted in decreasing
-    order.
+    '''Create a Double Ended Queue, q that will store indexes of array elements.
+    The queue will store indexes of useful elements in every window and it will
+    maintain decreasing order of values from front to rear in q, i.e.,
+    arr[q.front[]] to arr[q.rear()] are sorted in decreasing order.
 
     Args:
         arr(list): input array of integers
@@ -45,37 +41,30 @@ def printMax(arr, n, k):
     '''
     q = deque()
 
-    # Process first k (or first window)
-    # elements of array
+    # Process first k (or first window) elements of array
     for i in range(k):
 
-        # For every element, the previous
-        # smaller elements are useless
-        # so remove them from q
+        # For every element, the previous smaller elements are useless so remove
+        # them from q
         while q and arr[i] >= arr[q[-1]] :
             q.pop()
 
         # Add new element at rear of queue
         q.append(i)
 
-    # Process rest of the elements, i.e.
-    # from arr[k] to arr[n-1]
+    # Process rest of the elements, i.e. from arr[k] to arr[n-1]
     for i in range(k, n):
 
-        # The element at the front of the
-        # queue is the largest element of
+        # The element at the front of the queue is the largest element of
         # previous window, so print it
         print(str(arr[q[0]]) + " ", end = "")
 
-        # Remove the elements which are
-        # out of this window
+        # Remove the elements which are out of this window
         while q and q[0] <= i-k:
-
             # remove from front of deque
             q.popleft()
 
-        # Remove all elements smaller than
-        # the currently being added element
+        # Remove all elements smaller than the currently being added element
         # (Remove useless elements)
         while q and arr[i] >= arr[q[-1]] :
             q.pop()
